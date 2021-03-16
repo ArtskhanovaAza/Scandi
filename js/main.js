1878db43f2715aec1184ab14118cf2a76c7c2b6a
ArtskhanovaAza <69339977+ArtskhanovaAza@users.noreply.github.com>
2021-03-17 01:14:42 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(function () {
+	$('.reviews__slider-inner').slick({
+		responsive: [{
+			breakpoint: 1280,
+			settings: {
+				arrows: false,
+				dots: true
+			}
+		}]
+	});
+
+	$('.header__burger').on('click', (event) => {
+		$('.header__burger,.menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+
+
+
+	$("a[href^='#']").click(function () {
+		var _href = $(this).attr("href");
+		$("html, body").animate({
+			scrollTop: $(_href).offset().top + "px"
+		});
+		return false;
+	});
+});
